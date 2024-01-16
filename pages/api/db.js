ec2d251f315741993a30073755ec9f819bb487ff
kpunno <kpunno@myseneca.ca>
2024-01-16 17:49:10 -0500

    establish connection with mongodb

--- pages/api/db.js
+++ pages/api/db.js
@@ -1,7 +1,45 @@
-
 // api for db connection
 
-export default function handler(req,res) {
-    res.status(200).json({message: "hello from the api endpoint."});
+// require dotenv, acquire environment variables
+require('dotenv').config();
+const username = process.env.DB_USER;
+const password = process.env.DB_PASS;
+
+// database secret URI (handwritten to .env)
+const CONNECTION_STRING = `mongodb+srv://${username}:${password}@cluster0.bcearzi.mongodb.net/`;
+
+// set up an instance of mongo client
+const { MongoClient } = require('mongodb');
+
+// create a client object, pass the connection string
+const client = new MongoClient(CONNECTION_STRING);
+
+
+export default async function main() {
+    // await connecting to the database server via the client
+    await client.connect();
+    console.log("successful connection ...");
+    
+    const db = client.db("sample_mflix");
+    const collection = db.collection("users");
+
+    const findResult = await collection.find({}).toArray();
+    console.log('found all of this stuff:', findResult);
+
 }
+
+main()
+    .then(console.log)
+    .catch(console.error)
+    .finally(await client.close());
+
+//
+// TEST BELOW
+//
+
+/*
+export default function handler(req,res) {
+    res.status(200).json({message: CONNECTION_STRING});
+}
+*/
 
