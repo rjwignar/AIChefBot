59a913435a6a2ea62d1e3e6b9b1991bd5ffd1c1a
JohnPhillipO <jostiano@gmail.com>
2024-01-18 13:49:31 -0500

    added landing page

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
-import '@/styles/globals.css'
+import '@/styles/App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Layout from '@/components/Layout';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  )
 }
