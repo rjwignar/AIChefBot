d618f3c949d05a941b761946817b5efc328460c4
kpunno <kpunno@myseneca.ca>
2024-01-18 19:57:20 -0500

    improve response time, reorganize

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,12 +1,46 @@
-
 // performs interactions with the database
 
+
+// set up an instance of mongo client
+const { MongoClient } = require("mongodb");
+
+// require dotenv, acquire environment variables
+require("dotenv").config();
+
+// create a client object, pass the connection string
+const client = new MongoClient(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bcearzi.mongodb.net/`);
+
 // get one user
-module.exports.getUser = () => {
-    console.log("hello from getUser()")
-}
+module.exports.getUser = async () => {
+    // get users collection
+    const collection = client.db("aichefbot").collection("users");
+
+    const findResult = await collection
+      .find({ full_name: "exemplary example" })
+      .toArray();
+    //
+
+    // debug log the returned value from collection
+    console.debug(findResult);
+    return 1;
+};
 
 // add one user
 module.exports.addUser = () => {
-    console.log("hello from addUser()");
-}
+  connect();
+  console.log("hello from addUser()");
+};
+
+// connect to mongodb
+async function connect() {
+  try {
+    // attempt to connect
+    await client.connect();
+    // everything ok
+    console.debug("Connection established.")
+  } catch (err) {
+    // failed to connect
+    console.error(err);
+}};
+
+module.exports.connect = connect;
--- pages/recipes.js
+++
@@ -1,4 +0,0 @@
-// serves as a UI for testing the mongodb connection
-
-
-
