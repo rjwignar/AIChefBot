c1ae4db336a73452bb02b97768ca1dfc9293a22a
Roy Jeffrey Wignarajah <rjwignarajah@myseneca.ca>
2024-01-25 20:25:47 -0500

    added a redirection for /logout callback in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  redirects: async () => {
+    return [
+      {
+        source: "/logout",
+        destination: `${process.env.AWS_COGNITO_HOSTED_UI_DOMAIN}/logout?client_id=${process.env.AWS_COGNITO_CLIENT_ID}&logout_uri=${process.env.OAUTH_SIGN_OUT_REDIRECT_URL}&redirect_uri=${process.env.OAUTH_SIGN_IN_REDIRECT_URL}&response_type=code`,
+        permanent: false,
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
