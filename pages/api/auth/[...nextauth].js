06a8b0c0e3d15bb833c75ebb8932bd3773fe39ae
Roy Jeffrey Wignarajah <rjwignarajah@myseneca.ca>
2024-01-27 17:44:22 -0500

    drafted jwt strategy in nextauthjs config

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -14,6 +14,20 @@ export const authOptions = {
           }
         })
       ],
+    callbacks: {
+        async jwt(token, user, account, profile, isNewUser) {
+            // Called whenever a JWT is created/refreshed.
+            if (account?.id) {
+                token.id = account.id;
+            }
+            return token;
+        },
+        async session(session, token) {
+            // Called whenever a session is created/retrieved.
+            session.id = token.id;
+            return session;
+        },
+    },
 }
 
 export default NextAuth(authOptions)
