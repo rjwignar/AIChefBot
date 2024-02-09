dc70cfe817f74bfc877cc555e8f44a16756af8ce
kpunno <kpunno@myseneca.ca>
2024-02-08 23:41:29 -0500

    use account.js to fetch a user (account statistics)

--- components/MainNav.js
+++ components/MainNav.js
@@ -1,47 +1,11 @@
 import { Container, Nav, Navbar, NavDropdown, Image } from 'react-bootstrap';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
 import { useSession, signIn, signOut } from 'next-auth/react';
 
 export default function MainNav() {
    const router = useRouter();
    const { data: session, status } = useSession();
-   const [user, setUser] = useState(null);
-   // may need to fill the dependency array with [session], unless MainNav carries data between renders
-   useEffect(() => {
-      if (session != null) {
-         // get data
-         const fetchData = async () => {
-            const res = await fetch(`/api/request?id=${session.user.id}`, {
-               method: "GET",
-            });
-            await res.json().then(user => console.log("User exists: \n", user));
-            if (!res.ok) {
-               try {
-                  // post session to api
-                  const res = await fetch(`/api/request`, {
-                     method: "POST",
-                     headers: {
-                       "Content-Type": "application/json",
-                     },
-                     body: JSON.stringify(session.user),
-                   });
-                  const result = await res.json();
-   
-                   // log result for testing
-                  console.log("Added new user: \n", result);
-                  setUser(result);
-               }
-               catch(err) {
-                  // log errors for testing
-                  console.log(err);
-               }
-            }
-         }
-         fetchData();
-      }
-   }, []);
 
    return (
       <>
