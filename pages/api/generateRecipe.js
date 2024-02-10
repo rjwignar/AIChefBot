bee4361a9538e15e0f61fdad03a8c78fe68de235
Roy Jeffrey Wignarajah <rjwignarajah@myseneca.ca>
2024-02-10 16:22:12 -0500

    added messageHistory, modified response to send recipes only

--- pages/api/generateRecipe.js
+++ pages/api/generateRecipe.js
@@ -3,6 +3,9 @@ import OpenAI from 'openai';
 
 // API calls to /api/generateRecipe
 export default async function handler(req, res) {
+    const messageHistory = [
+
+    ]
     if (req.method === 'POST') {
         try {
             const openai = new OpenAI({ apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY });
@@ -19,10 +22,26 @@ export default async function handler(req, res) {
                 messages: [{ role: "user", content: prompt }],
                 model: "gpt-3.5-turbo-1106",
             });
+
+            // store response
+            const response = completion.choices[0];
+            // append initial message prompt to messageHistory
+            messageHistory.push({ role: "user", content: prompt});
+            // append initial LLM response to messageHistory
+
+            const llmResponse = response.message;
+            messageHistory.push({ role: "assistant", content: llmResponse });
+
+            // take recipes only
+            const recipes = response.message.content;
+            console.log(recipes);
             console.log('Server response:', completion); // Add this line to log the response
             console.log('Message below', completion.choices[0].message);
             console.log(completion.choices[0]);
-            res.status(200).json(completion.choices[0]);
+
+            console.log("Message History", messageHistory);
+            // push recipes
+            res.status(200).json(recipes);
         } catch (error) {
             console.error('Error fetching recipes:', error);
             res.status(500).json({ error: 'Error fetching recipes' });
