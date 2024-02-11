50662334e185dec832c709f22f688ff5762a6089
Roy Jeffrey Wignarajah <rjwignarajah@myseneca.ca>
2024-02-11 16:44:43 -0500

    updated generatePageContent

--- components/generatedRecipes.js
+++ components/generatedRecipes.js
@@ -48,9 +48,37 @@ const GeneratePageContent = ({ selectedDiet }) => {
 
   const handleGenerateMoreClick = async () => {
     try {
-      console.log()
-    } catch (error) {
+      // console.log('diet below');
+      // console.log(selectedDiet);
+      console.log(messageHistory);
+      const response = await fetch('/api/generateRecipe', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ messageHistory }),
+      });
 
+      if (!response.ok) {
+        throw new Error(`Network response was not ok: ${response.statusText}`);
+      }
+
+      const contentType = response.headers.get('content-type');
+      if (!contentType || !contentType.includes('application/json')) {
+        throw new TypeError("Oops, we haven't got JSON!");
+      }
+
+      const data = await response.json();
+      console.log(data);
+      // const {receivedRecipes, updatedMessageHistory} = data;
+      setRecipes(data.recipes);
+      // setRecipes(receivedRecipes);
+      console.log("message history", data.messageHistory);
+      setMessageHistory(data.messageHistory);
+      console.log("new message history", messageHistory);
+      console.log("recipes below in UI", recipes);
+    } catch (error) {
+      console.error('Error fetching recipes:', error);
     }
   };
   return (
@@ -65,7 +93,7 @@ const GeneratePageContent = ({ selectedDiet }) => {
         <RecipeCards recipes={recipes} />
       </div>}
       {recipes &&
-        <Button variant="success" size="lg" onClick={handleGenerateClick}>
+        <Button variant="success" size="lg" onClick={handleGenerateMoreClick}>
           Generate Similar Recipes
         </Button>
       }
