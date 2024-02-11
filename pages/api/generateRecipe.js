8cef47ac5a53b65b6296c08eeb883c8cc92c366a
Roy Jeffrey Wignarajah <rjwignarajah@myseneca.ca>
2024-02-11 13:29:27 -0500

    extracted generateRecipes to generateRecipeUtils

--- pages/api/generateRecipe.js
+++ pages/api/generateRecipe.js
@@ -1,88 +1,51 @@
 // pages/api/generateRecipe.js
-import OpenAI from 'openai';
-import { generateDietPrompt, repeatPrompt } from './generateRecipeUtils.js';
+import { generateDietPrompt, generateRecipes, repeatPrompt } from './generateRecipeUtils.js';
+
 // API calls to /api/generateRecipe
 export default async function handler(req, res) {
 
-    const generateRecipes = async (prompt, messageHistory) => {
-        try {
-            // Initialize OpenAI object using OPENAI_API_KEY
-            const openai = new OpenAI({ apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY });
-
-
-            // Append initial message prompt to messageHistory
-            // NOTE: The prompt is the message content from user, so we must wrap it in { role: "user", content: prompt}
-            messageHistory.push({ role: "user", content: prompt });
-
-            // Send request to OpenAI API
-            // response_format of `json_object` guarantees JSON response from OpenAI API.
-            // However, you MUST instruct the model to produce JSON (specify JSON in initial prompt)
-            // NOTE: response_format property only supported by gpt4-turbo and all gpt-3.5-turbo models starting at gpt-3.5-turbo-1106 and later
-            // See https://platform.openai.com/docs/api-reference/chat/create#chat-create-response_format for more information
-            const completion = await openai.chat.completions.create({
-                response_format: { "type": "json_object" },
-                messages: messageHistory,
-                model: "gpt-3.5-turbo-1106",
-            });
-
-            // Store OpenAI response
-            const response = completion.choices[0];
-
-            // Append initial LLM response to messageHistory
-            // NOTE: response.message is already in  { role, content } format, so no need to wrap it in JSON
-            const llmResponse = response.message;
-            messageHistory.push(llmResponse);
-
-
-            // Extract list of recipes from response
-            const recipes = response.message.content;
-
-            // Console Logging
-            console.log(recipes);
-            console.log('Server response:', completion); // Add this line to log the response
-            console.log('Message below', completion.choices[0].message);
-            console.log(completion.choices[0]);
-            console.log('llm response', llmResponse);
-            console.log("Message History", messageHistory);
-
-            // Push recipes and messageHistory in response
-            res.status(200).json({ recipes, messageHistory });
-        } catch (error) {
-            console.error('Error fetching recipes:', error);
-            res.status(500).json({ error: 'Error fetching recipes' });
-        }
-    };
-
     if (req.method === 'POST') {
-        // User is generating more recipes based on their initial recipe/diet constraints
-        if (req.body.messageHistory.length > 0){
-            // destructure messageHistory from req.body as it's the only property of req.body
-            const {messageHistory} = req.body;
-            console.log("here is message history",messageHistory);
+        try{
 
-            // Send repeatPrompt so LLM reuses original initial instructions
-            generateRecipes(repeatPrompt, messageHistory);
+            // User is generating more recipes based on their initial recipe/diet constraints
+            if (req.body.messageHistory.length > 0){
+                // destructure messageHistory from req.body as it's the only property of req.body
+                const {messageHistory} = req.body;
+                console.log("here is message history",messageHistory);
+                
+                // Send repeatPrompt so LLM reuses original initial instructions
+                const response = await generateRecipes(repeatPrompt, messageHistory);
+                
+                // Push recipes and messageHistory in response
+                res.status(200).json(response);
+            }
+            else{
+                // User selected a diet and starts generating recipes
+                if (req.body.hasOwnProperty('selectedDiet')){
+                    console.log("User has started generating recipes by diet!");
+                    
+                    // destructure selectedDiet and messageHistory properties from req.body
+                    const { selectedDiet, messageHistory } = req.body;
+                    
+                    const response = await generateRecipes(generateDietPrompt(selectedDiet), messageHistory);
+                    
+                    // Push recipes and messageHistory in response
+                    res.status(200).json(response);
+                }
+                // User selected list of ingredients and starts generating recipes
+                else if (req.body.hasOwnProperty('selectedIngredients')){
+                    console.log("User has started generating recipes by ingredients!");
+                }
+                // User selected list of recipes from recipe manager and is generating recipes
+                else if (req.body.hasOwnProperty('selectedRecipes')){
+                    console.log("User is generating similar recipes from a selection in their recipe management page!");
+                }
+            }
+            
+        } catch(error){
+            console.error('Error fetching recipes:', error);
+            res.status(500).json({ error: error.message });
         }
-        else{
-            // User selected a diet and starts generating recipes
-            if (req.body.hasOwnProperty('selectedDiet')){
-                console.log("User has started generating recipes by diet!");
-
-                // destructure selectedDiet and messageHistory properties from req.body
-                const { selectedDiet, messageHistory } = req.body;
-
-                generateRecipes(generateDietPrompt(selectedDiet), messageHistory);
-            }
-            // User selected list of ingredients and starts generating recipes
-            else if (req.body.hasOwnProperty('selectedIngredients')){
-                console.log("User has started generating recipes by ingredients!");
-            }
-            // User selected list of recipes from recipe manager and is generating recipes
-            else if (req.body.hasOwnProperty('selectedRecipes')){
-                console.log("User is generating similar recipes from a selection in their recipe management page!");
-            }
-        }
-
         // console.log("request body message history length", req.body.messageHistory.length);
         // console.log("request body", req.body);
         // console.log("is there a selectedDiet property", req.body.hasOwnProperty('selectedDiet'));
--- pages/api/generateRecipeUtils.js
+++ pages/api/generateRecipeUtils.js
@@ -15,3 +15,49 @@ export const repeatPrompt =
 `Generate three more unique recipes that satisfy the original requirements defined.
 Recipes must be returned in a JSON object with the same properties as before.
 Do not number the steps, and be descriptive while minimizing token usage.`;
+
+export async function generateRecipes(prompt, messageHistory) {
+    try {
+        // Initialize OpenAI object using OPENAI_API_KEY
+        const openai = new OpenAI({ apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY });
+
+        // Append initial message prompt to messageHistory
+        // NOTE: The prompt is the message content from user, so we must wrap it in { role: "user", content: prompt}
+        messageHistory.push({ role: "user", content: prompt });
+
+        // Send request to OpenAI API
+        // response_format of `json_object` guarantees JSON response from OpenAI API.
+        // However, you MUST instruct the model to produce JSON (specify JSON in initial prompt)
+        // NOTE: response_format property only supported by gpt4-turbo and all gpt-3.5-turbo models starting at gpt-3.5-turbo-1106 and later
+        // See https://platform.openai.com/docs/api-reference/chat/create#chat-create-response_format for more information
+        const completion = await openai.chat.completions.create({
+            response_format: { "type": "json_object" },
+            messages: messageHistory,
+            model: "gpt-3.5-turbo-1106",
+        });
+
+        // Store OpenAI response
+        const response = completion.choices[0];
+
+        // Append initial LLM response to messageHistory
+        // NOTE: response.message is already in  { role, content } format, so no need to wrap it in JSON
+        const llmResponse = response.message;
+        messageHistory.push(llmResponse);
+
+        // Extract list of recipes from response
+        const recipes = response.message.content;
+
+        // Console Logging
+        console.log(recipes);
+        console.log('Server response:', completion); // Add this line to log the response
+        console.log('Message below', completion.choices[0].message);
+        console.log(completion.choices[0]);
+        console.log('llm response', llmResponse);
+        console.log("Message History", messageHistory);
+
+        // Return recipes and messageHistory as JSON
+        return {recipes, messageHistory};
+    } catch (error) {
+        throw new Error(error);
+    }
+};
