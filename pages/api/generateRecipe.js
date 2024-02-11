1876444213b2513f598686aaab009a3680e78e0b
Roy Jeffrey Wignarajah <rjwignarajah@myseneca.ca>
2024-02-11 13:04:17 -0500

    moved generateDietPrompt and repeatPrompt to pages/api/generateRecipeUtils.js

--- pages/api/generateRecipe.js
+++ pages/api/generateRecipe.js
@@ -1,24 +1,8 @@
 // pages/api/generateRecipe.js
 import OpenAI from 'openai';
-
+import { generateDietPrompt, repeatPrompt } from './generateRecipeUtils.js';
 // API calls to /api/generateRecipe
 export default async function handler(req, res) {
-    function generateDietPrompt(selectedDiet){
-        const dietPrompt =
-        `Generate three recipes based on the following diet: ${selectedDiet}.
-        Recipes must be returned in a JSON object, where each recipe contains the following properties:
-        id (string), name (string), ingredients (string array), ingredientQuantity (string array), steps (string array)
-        Each id is a unique, randomized alphanumeric with exactly 10 characters. 
-        Do not number the steps, but minimize token usage by giving concise steps.`;
-
-        return dietPrompt;
-    }
-
-    const repeatPrompt =
-    `Generate three more unique recipes that satisfy the original requirements defined.
-    Recipes must be returned in a JSON object with the same properties as before.
-    Do not number the steps, and be descriptive while minimizing token usage.`;
-
 
     const generateRecipes = async (prompt, messageHistory) => {
         try {
