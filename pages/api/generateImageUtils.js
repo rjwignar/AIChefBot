56d573c371fb07cbc9a05d52fcdbe7b697c11eaf
Roy Jeffrey Wignarajah <rjwignarajah@myseneca.ca>
2024-03-10 15:31:22 -0400

    drafted generateOneRecipeImage

---
+++ pages/api/generateImageUtils.js
@@ -0,0 +1,19 @@
+// pages/api/generateImageUtils.js
+import OpenAI from 'openai';
+
+const openai = new OpenAI({apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY})
+
+const generateOneRecipeImage = async (recipe) =>{
+    const imagePrompt = `${recipe.name}, prepared using the following ingredients: ${recipe.ingredients}.
+    The dish is beautifully plated and presented as though featured on a high-end restaurant menu.`
+    const image = await openai.images.generate({
+        model: "dall-e-2",
+        prompt: imagePrompt,
+    });
+
+    // give the recipe object a new property, imageURL
+    recipe.imageURL = image.data[0].url;
+
+    // return updated recipe object
+    return recipe;
+}
