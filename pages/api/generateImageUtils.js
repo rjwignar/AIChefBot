2e86b5081bf73c7372ab5cb412e5df87ae731081
Roy Jeffrey Wignarajah <rjwignarajah@myseneca.ca>
2024-03-10 15:45:27 -0400

    added try/catch to generateOneRecipeImage(), updated comments

--- pages/api/generateImageUtils.js
+++ pages/api/generateImageUtils.js
@@ -4,27 +4,33 @@ import OpenAI from 'openai';
 const openai = new OpenAI({apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY})
 
 const generateOneRecipeImage = async (recipe) =>{
-    const imagePrompt = `${recipe.name}, prepared using the following ingredients: ${recipe.ingredients}.
-    The dish is beautifully plated and presented as though featured on a high-end restaurant menu.`
-    const image = await openai.images.generate({
-        model: "dall-e-2",
-        prompt: imagePrompt,
-    });
-
-    // give the recipe object a new property, imageURL
-    recipe.imageURL = image.data[0].url;
-
-    // return updated recipe object
-    return recipe;
+    try{
+        const imagePrompt = `${recipe.name}, prepared using the following ingredients: ${recipe.ingredients}.
+        The dish is beautifully plated and presented as though featured on a high-end restaurant menu.`
+        const image = await openai.images.generate({
+            model: "dall-e-2",
+            prompt: imagePrompt,
+        });
+        
+        // give the recipe object a new property, imageURL
+        recipe.imageURL = image.data[0].url;
+        
+        // return updated recipe object
+        return recipe;
+    } catch (error) {
+        throw new Error(error);
+    }
 }
 
 export async function generateRecipeImages(recipes){
     try{
+        // make parallel calls to generateOneRecipeImage to add an AI-generate image to each recipe
         const recipesWithImagesPromise = recipes.map(recipe => generateOneRecipeImage(recipe));
         const recipesWithImages = await Promise.all(recipesWithImagesPromise);
 
         console.log("array of recipes with images", recipesWithImages);
 
+        // return array of recipes, now with an AI-generated image
         return recipesWithImages;
     } catch (error) {
         throw new Error(error);
