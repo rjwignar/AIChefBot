65c2dad0de25f15795dc92c7e5048065d23218ac
Roy Jeffrey Wignarajah <rjwignarajah@myseneca.ca>
2024-03-11 18:01:50 -0400

    formatted images/request.js

--- pages/api/images/request.js
+++ pages/api/images/request.js
@@ -5,41 +5,41 @@ import { addImage, deleteImage } from "./images";
 
 // handler for all relevant requests to /api/images/request
 async function handler(req, res) {
-  switch (req.method) {
-    // POST
-    case "POST": {
-      try {
-        // Add recipe image
-        try {
-          const imageDetails = await addImage(req.body.imageURL);
-          res.status(201).json({secure_url: imageDetails.secure_url, public_id: imageDetails.public_id});
-        } catch (err) {
-          console.error(err);
-        }
-      } catch (err) {
-        res.status(500).json({message: "Failed to add recipe image.", "Error: ": err});
-      }
-      break;
-    }
-
-    // DELETE
-    case "DELETE": {
-        try {
-            // Await recipe deletion
-            const result = await deleteImage(req.body);
-            
-            if (result === "ok") {
-                // image successfully deleted
-                res.status(204).json({message: "Recipe image successfully removed."});
-            } else {
-                res.status(404).json({ message: "Recipe image was not deleted." });
+    switch (req.method) {
+        // POST
+        case "POST": {
+            try {
+                // Add recipe image
+                try {
+                    const imageDetails = await addImage(req.body.imageURL);
+                    res.status(201).json({ secure_url: imageDetails.secure_url, public_id: imageDetails.public_id });
+                } catch (err) {
+                    console.error(err);
+                }
+            } catch (err) {
+                res.status(500).json({ message: "Failed to add recipe image.", "Error: ": err });
             }
-        } catch (err) {
-            res.status(500).json({message: "Failed to delete recipe image.", "Error: ": err});
+            break;
         }
-        break;
+
+        // DELETE
+        case "DELETE": {
+            try {
+                // Await recipe deletion
+                const result = await deleteImage(req.body);
+
+                if (result === "ok") {
+                    // image successfully deleted
+                    res.status(204).json({ message: "Recipe image successfully removed." });
+                } else {
+                    res.status(404).json({ message: "Recipe image was not deleted." });
+                }
+            } catch (err) {
+                res.status(500).json({ message: "Failed to delete recipe image.", "Error: ": err });
+            }
+            break;
+        }
     }
-  }
 }
 
 export default handler;
