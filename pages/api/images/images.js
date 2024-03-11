9d1f0c673720cf1eba03ec31ed50c0438fb00885
Roy Jeffrey Wignarajah <rjwignarajah@myseneca.ca>
2024-03-11 15:42:26 -0400

    debugged addImage() using Promise syntax

--- pages/api/images/images.js
+++ pages/api/images/images.js
@@ -8,21 +8,22 @@ cloudinary.config({
 });
 
 export async function addImage(imageURL) {
-    try {
-        cloudinary.uploader.upload(
-            imageURL,
-            {},
-            function (error, result) {
-                console.log(result);
-
-                // destructure secure_url and public_id from result
-                const { secure_url, public_id } = result;
-                console.log("image url and id to be returned as JSON", {secure_url, public_id});
-                return {secure_url, public_id};
-            }
-        );
-    } catch (err) {
-        console.error(err);
-        return null;
-    }
+        return new Promise((resolve, reject) => {
+            cloudinary.uploader.upload(
+                imageURL,
+                {},
+                function (err, result) {
+                    if (err) {
+                        console.error(err);
+                        reject(err);
+                    } else {
+                        console.log("result", result);
+                        // destructure secure_url and public_id from result
+                        const { secure_url, public_id } = result;
+                        console.log("image url and id to be returned as JSON", { secure_url, public_id });
+                        resolve({ secure_url, public_id });
+                    }
+                }
+            );
+        });
 }
