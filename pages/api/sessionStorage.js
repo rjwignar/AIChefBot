46ebd91315c9e56e9a44ece97849b8def86b3edf
Kristjan Punno <k.punno@gmail.com>
2024-03-25 20:56:20 -0400

    include message history and selectedDiet in cache

--- pages/api/sessionStorage.js
+++ pages/api/sessionStorage.js
@@ -3,22 +3,16 @@
 // a) User registers when they generate a recipe they like
 // b) User navigates from page and wants to see the same recipes
 
-export function cacheRecipes(recipes) {
-    console.log("Caching " + recipes.length + " recipes...");
-    for (let i=0; i < recipes.length; i++) {
-        sessionStorage.setItem(i, JSON.stringify(recipes[i]));
-    }
+export function setCache(recipes, messageHistory, selectedDiets) {
+    sessionStorage.setItem("recipes", JSON.stringify(recipes));
+    sessionStorage.setItem("selectedDiets", JSON.stringify(selectedDiets));
+    sessionStorage.setItem("messageHistory", JSON.stringify(messageHistory));
 }
 
-export function fetchCachedRecipes() {
-    console.log("Fetching recipes from cache...");
-    let recipes = []
-    for (let i=0; i < 3; i++) {
-        const recipeString = sessionStorage.getItem(i);
-        if (recipeString) {
-            recipes.push(JSON.parse(recipeString));
-        }
-    }
-    console.log(recipes.length ? recipes.length : 0 + " recipes found!");
-    return recipes;
+export function getCache() {
+    return [
+        sessionStorage.getItem("recipes"),
+        sessionStorage.getItem("selectedDiets"),
+        sessionStorage.getItem("messageHistory")
+    ]
 }
