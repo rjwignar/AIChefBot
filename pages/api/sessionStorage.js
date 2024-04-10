5ab219edafea7f5814e3f1883f45904914f55762
Kristjan Punno <k.punno@gmail.com>
2024-04-09 20:35:19 -0400

    correct undefined as boolean false

--- pages/api/sessionStorage.js
+++ pages/api/sessionStorage.js
@@ -9,9 +9,11 @@ export function setCache(data) {
     // Which page were the recipes generated from?
     // If both are true, ingredients + diet
     // Otherwise, ingredients OR diet
+
+    // Property could be undefined, so we use false for undefined
     sessionStorage.setItem('selectedIngredients', data.selectedIngredients ? true : false);
     sessionStorage.setItem('selectedDiet', data.selectedDiet ? true : false);
-    sessionStorage.setItem('similarRecipes', data.similarRecipes);
+    sessionStorage.setItem('similarRecipes', data.similarRecipes ? true : false);
 }
 
 // Array of JSON objects must be parsed
@@ -21,6 +23,6 @@ export function getCache() {
         messageHistory: JSON.parse(sessionStorage.getItem("messageHistory")),
         selectedIngredients: JSON.parse(sessionStorage.getItem("selectedIngredients")),
         selectedDiet: JSON.parse(sessionStorage.getItem("selectedDiet")),
-        similarRecipes: JSON.parse(sessionStorage.getItem('similarRecipes')),
+        similarRecipes: JSON.parse(sessionStorage.getItem("similarRecipes")),
     }
 }
