848816d53f660262a2b8d297373b045c6117c962
Kristjan Punno <k.punno@gmail.com>
2024-04-11 12:03:15 -0400

    refactor generateRecipe.js and generateRecipeUtils.js

--- pages/api/generateRecipe.js
+++ pages/api/generateRecipe.js
@@ -1,6 +1,7 @@
 // pages/api/generateRecipe.js
 import { NextResponse } from 'next/server.js';
 import { generateDietPrompt, generateIngredientsPrompt, generateIngredientsWithDietPrompt, generateSimilarRecipesPrompt, generateRecipes, repeatPrompt } from './generateRecipeUtils.js';
+import { generatePrompt } from './generateRecipeUtils.js';
 
 // Use Edge runtime when deployed on Vercel instead of serverless runtime to avoid 10-second timeout limit when generating recipes
 export const config = {
@@ -25,8 +26,6 @@ function inspectRequestBody(requestBody) {
     console.log("---------------------------------------------------");
 }
 
-// SPRINT 3 TODO: INTEGRATE WITH RECIPE MANAGEMENT FRONTEND BY ADDING API CALL TO APPROPRIATE BUTTON ONCLICK
-// API calls to /api/generateRecipe
 const handler = async (req, res) => {
     // Assign request body based on serverless vs. Edge runtime
     const requestBody = isEdgeRuntime ? await req.json() : req.body;
@@ -35,108 +34,26 @@ const handler = async (req, res) => {
     inspectRequestBody(requestBody);
 
     isEdgeRuntime ? console.log("Edge runtime detected") : console.log("Serverless runtime detected");
+
     if (req.method === 'POST') {
         try {
-            // User is generating more recipes based on their initial recipe/diet constraints
-            if (requestBody.messageHistory.length > 0) {
+            // Get message history
+            const { messageHistory } = requestBody;
+            // Generate a prompt
+            const prompt = generatePrompt(requestBody);
+            console.log("Generate prompt: ", prompt);
+            // Generate recipes using prompt and message history
+            const response = await generateRecipes(prompt, messageHistory);
 
-                // destructure messageHistory from requestBody as it's the only property of requestBody
-                const { messageHistory } = requestBody;
-                console.log("here is message history", messageHistory);
-
-                // We use repeatPrompt to instruct LLM to reuse original instructions from initial request
-                // Pass this along with messageHistory to LLM
-                const response = await generateRecipes(repeatPrompt, messageHistory);
-
-                // Push recipes and messageHistory in response
-                if (isEdgeRuntime) {
-                    return NextResponse.json(response);
-                }
-                else {
-                    res.status(200).json(response);
-                }
+            // ???
+            if (isEdgeRuntime) {
+                return NextResponse.json(response);
             }
             else {
-                if (requestBody.hasOwnProperty('selectedDiet') && requestBody.hasOwnProperty('selectedIngredients')) {
-                    console.log("User has started generating recipes by ingredients and DIET!!!!!!!!!!!!!!!!");
-
-                    // destructure selectedDiet and messageHistory properties from requestBody
-                    const { selectedDiet, selectedIngredients, limitIngredients, messageHistory } = requestBody;
-
-                    // Generate Diet prompt from selectedDiet
-                    // Then pass it along with messageHistory to the LLM
-                    const response = await generateRecipes(generateIngredientsWithDietPrompt(selectedIngredients, selectedDiet, limitIngredients), messageHistory);
-
-                    // Push recipes and messageHistory in response
-                    if (isEdgeRuntime) {
-                        return NextResponse.json(response);
-                    }
-                    else {
-                        res.status(200).json(response);
-                    }
-
-                }
-                // User selected a diet and starts generating recipes
-                else if (requestBody.hasOwnProperty('selectedDiet')) {
-                    console.log("User has started generating recipes by diet!");
-
-                    // destructure selectedDiet and messageHistory properties from requestBody
-                    const { selectedDiet, messageHistory } = requestBody;
-
-                    // Generate Diet prompt from selectedDiet
-                    // Then pass it along with messageHistory to the LLM
-                    const response = await generateRecipes(generateDietPrompt(selectedDiet), messageHistory);
-
-                    // Push recipes and messageHistory in response
-                    if (isEdgeRuntime) {
-                        return NextResponse.json(response);
-                    }
-                    else {
-                        res.status(200).json(response);
-                    }
-                }
-                // User selected list of ingredients and starts generating recipes
-                else if (requestBody.hasOwnProperty('selectedIngredients')) {
-                    console.log("User has started generating recipes by ingredients!");
-
-                    // destructure selectedIngredients and messageHistory properties from requestBody
-                    const { selectedIngredients, limitIngredients, messageHistory } = requestBody;
-
-                    // Generate Ingredients Prompt from selectedIngredients
-                    // Then pass it along with messageHistory to the LLM
-                    const response = await generateRecipes(generateIngredientsPrompt(selectedIngredients, limitIngredients), messageHistory);
-
-                    // Push recipes and messageHistory in response
-                    if (isEdgeRuntime) {
-                        return NextResponse.json(response);
-                    }
-                    else {
-                        res.status(200).json(response);
-                    }
-                }
-
-                // User selected list of recipes from recipe manager and is generating recipes
-                else if (requestBody.hasOwnProperty('selectedRecipes')) {
-                    console.log("User is generating similar recipes from a selection in their recipe management page!");
-
-                    // destructure selectedRecipes and messageHistory properties from requestBody
-                    const { selectedRecipes, messageHistory } = requestBody;
-
-                    // Generate Similar Recipes Prompt from selectedRecipes
-                    // Then pass it along with messageHistory to the LLM
-                    const response = await generateRecipes(generateSimilarRecipesPrompt(selectedRecipes), messageHistory);
-
-                    // Push recipes and messageHistory in response
-                    if (isEdgeRuntime) {
-                        return NextResponse.json(response);
-                    }
-                    else {
-                        res.status(200).json(response);
-                    }
-                }
-            }
-
+                res.status(200).json(response);
+            }            
         } catch (error) {
+            // Bad response or error
             console.error('Error fetching recipes:', error);
             if (isEdgeRuntime) {
                 return NextResponse.json({ error: error.message }, { status: 500 });
--- pages/api/generateRecipeUtils.js
+++ pages/api/generateRecipeUtils.js
@@ -30,46 +30,56 @@ Each ingredient must include the ingredient's name, quantity, and if applicable,
 Each step must be comma-separated. Do not number the steps.
 When applicable, include durations in a step (e.g. boil for 10 minutes).`;
 
-export function generateDietPrompt(selectedDiet) {
-    return `Generate ${recipeCount} recipes based on the following diet: ${selectedDiet}.\n` +
-        recipeRequirements;
-}
+export function generatePrompt(requestBody) {
+    // Destructure all
+    const { 
+        messageHistory,
+        selectedDiet,
+        selectedIngredients,
+        limitIngredients,
+        selectedRecipes,
+    } = requestBody;
 
-export function generateIngredientsPrompt(selectedIngredients, limitIngredients) {
-    return `Generate ${recipeCount} recipes based on the following list of ingredients. 
-        (Ignore any invalid ingredients or ingredients that doesn't make sense): ${selectedIngredients}.\n ${limitIngredients ? 
-        `ONLY use the ingredients that I listed to generate the recipes. Don't use or add any other ingredients other than the 
-        list I mentioned. Recipes should only be created using the listed ingredients.\n` : `Use the ingredients I listed and Add other ingredients that I haven't listed.\n`
-        }` + recipeRequirements + 
-        `If there is any invalid ingredients or ingredients that doesn't make sense (i.e. any electronic or unwanted objects, any empty string, words, etc...) then 
-        ignore it and generate random recipes unless if there is any valid ingredients then use that to generate recipes instead of the invalid ingredients`;
+    // If message history, return a repeat prompt
+    if (messageHistory > 0) {
+        return `Generate ${recipeCount} more unique recipes that satisfy the original requirements defined.
+                Recipes must be returned in a JSON object with the same properties as before.
+                Do not number the steps, and be descriptive while minimizing token usage.`;
+    }
+    // If selected diet and ingredients, return prompt that ...
+    if (selectedDiet && selectedIngredients) {
+        return `Generate ${recipeCount} recipes based on the following list of ingredients, enuring they align with the selected diet preference (${selectedDiet}).
+                (Ignore any invalid ingredients or combinations that don't match the specified diet): ${selectedIngredients}.\n ${limitIngredients ? 
+                `ONLY use the ingredients that I listed to generate the recipes. Don't use or add any other ingredients other than the 
+                list I mentioned. Recipes should only be created using the listed ingredients.\n` : `Use the ingredients I listed and Add other ingredients that I haven't listed.\n`
+                }` + recipeRequirements + 
+                `If there are any ingredients that conflict with the selected diet (${selectedDiet}), such as meat products in a vegetarian or pescatarian
+                diet, ensure that the generated recipes do not include those ingredients. Instead, substitute them with suitable alternatives that adhere 
+                to the chosen dietary restrictions.`;
+    }
+    // If selected diet, return prompt that ...
+    else if (selectedDiet) {
+        return `Generate ${recipeCount} recipes based on the following diet: ${selectedDiet}.\n` + recipeRequirements;
+    }
+    // If selected ingredients, return prompt that ...
+    else if (selectedIngredients) {
+        return `Generate ${recipeCount} recipes based on the following list of ingredients. 
+                (Ignore any invalid ingredients or ingredients that doesn't make sense): ${selectedIngredients}.\n ${limitIngredients ? 
+                `ONLY use the ingredients that I listed to generate the recipes. Don't use or add any other ingredients other than the 
+                list I mentioned. Recipes should only be created using the listed ingredients.\n` : `Use the ingredients I listed and Add other ingredients that I haven't listed.\n`
+                }` + recipeRequirements + 
+                `If there is any invalid ingredients or ingredients that doesn't make sense (i.e. any electronic or unwanted objects, any empty string, words, etc...) then 
+                ignore it and generate random recipes unless if there is any valid ingredients then use that to generate recipes instead of the invalid ingredients`;
+    }
+    // If selected recipes, return prompt that ...
+    else if (selectedRecipes) {
+        const selectedRecipesString = JSON.stringify({recipes: selectedRecipes}, null, 2);
+        return `Generate ${recipeCount} recipes that are similar to this list of recipes: ${selectedRecipesString}\n` + 
+                `Make sure that the recipes that are generated have some of the same ingredients in the new recipes.\n` +
+                recipeRequirements;
+    }
+    
 }
-
-export function generateIngredientsWithDietPrompt(selectedIngredients, selectedDiet, limitIngredients) {
-    return `Generate ${recipeCount} recipes based on the following list of ingredients, enuring they align with the selected diet preference (${selectedDiet}).
-        (Ignore any invalid ingredients or combinations that don't match the specified diet): ${selectedIngredients}.\n ${limitIngredients ? 
-        `ONLY use the ingredients that I listed to generate the recipes. Don't use or add any other ingredients other than the 
-        list I mentioned. Recipes should only be created using the listed ingredients.\n` : `Use the ingredients I listed and Add other ingredients that I haven't listed.\n`
-        }` + recipeRequirements + 
-        `If there are any ingredients that conflict with the selected diet (${selectedDiet}), such as meat products in a vegetarian or pescatarian
-        diet, ensure that the generated recipes do not include those ingredients. Instead, substitute them with suitable alternatives that adhere 
-        to the chosen dietary restrictions.`;
-}
-
-// Because selectedRecipes can contain an assortment of random recipes, we're not exactly sure what recipes will be produced with this current prompt
-// In Sprint 3, we will have to experiment with this prompt.
-// As well, we may want to generate a different number of recipes than we would if we were generating from strictly ingredients or diet
-export function generateSimilarRecipesPrompt(selectedRecipes) {
-    const selectedRecipesString = JSON.stringify({recipes: selectedRecipes}, null, 2);
-    return `Generate ${recipeCount} recipes that are similar to this list of recipes: ${selectedRecipesString}\n` + 
-            `Make sure that the recipes that are generated have some of the same ingredients in the new recipes.\n` +
-            recipeRequirements;
-}
-
-export const repeatPrompt =
-`Generate ${recipeCount} more unique recipes that satisfy the original requirements defined.
-Recipes must be returned in a JSON object with the same properties as before.
-Do not number the steps, and be descriptive while minimizing token usage.`;
 
 export async function generateRecipes(prompt, messageHistory) {
     try {
