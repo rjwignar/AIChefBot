6cf8971ec1d6bc752a1cf430c329d90e9d179dd8
Roy Jeffrey Wignarajah <rjwignarajah@myseneca.ca>
2024-04-13 15:44:05 -0400

    updated LoadingScreen component with conditional loadingText

--- components/LoadingScreen.js
+++ components/LoadingScreen.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { Container, Card, Row, Col } from 'react-bootstrap';
 
 // Makes the loading screen page when user is generating recipes.
-const LoadingScreen = () => {
+const LoadingScreen = ({awaitingImages}) => {
+   const loadingText = awaitingImages ? "Almost ready" : "Loading";
    return (
       <Container className="text-center my-4">
-         <h4 className="loading-text">Loading<span>.</span><span>.</span><span>.</span></h4>
+         <h4 className="loading-text">{loadingText}<span>.</span><span>.</span><span>.</span></h4>
          <br/>
          <Row className='animate__animated animate__fadeInUp'>
             {[...Array(3)].map((_, i) => (
