a3bf87c5feb6d693a4ce44568da025ab3568ceb0
Kristjan Punno <k.punno@gmail.com>
2024-04-15 09:40:46 -0400

    update text to be more descriptive

--- components/LoadingScreen.js
+++ components/LoadingScreen.js
@@ -3,11 +3,11 @@ import { Container, Card, Row, Col } from 'react-bootstrap';
 
 // Makes the loading screen page when user is generating recipes.
 const LoadingScreen = ({awaitingImages}) => {
-   const loadingText = awaitingImages ? "Almost ready" : "Loading";
+   const loadingText = awaitingImages ? "Almost ready! Getting your images" : "Generating your recipes...";
    return (
       <Container className="text-center my-4">
          <h4 className="loading-text">{loadingText}<span>.</span><span>.</span><span>.</span></h4>
-         <br/>
+         <br/><br/>
          <Row className='animate__animated animate__fadeInUp'>
             {[...Array(3)].map((_, i) => (
                <Col key={i} md={4} className="mb-4">
